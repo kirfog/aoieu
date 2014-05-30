cbac03058c9e5048a16da0297222a46782cc7c89
kirfog <kirfog@gmail.com>
2014-05-30 14:44:46 +0400

    html

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<form method="post">
+			<input type="text" name="input1" value="input1">
+			<input type="submit" name="submit1" value="submit1">
+		</form>
+	</body>
+</html>
 <?php
-phpinfo();
 ?>
