9dc501184e120908896385eb82ea803c64ad314d
kirfog <kirfog@gmail.com>
2015-03-13 00:09:04 +0300

    template0.001

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 $(document).ready(function() {
 	
-	$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
+$('body').panelSnap();
 	
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = 100;
+ //nav.outerHeight()
+ 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  sections.each(function() {
+	  
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').parent().removeClass('active');
+      sections.removeClass('active');
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+    }
 	
-	// navigation click actions	
-	$('.scroll-link').on('click', function(event){
-		event.preventDefault();
-		var sectionID = $(this).attr("data-id");
-		scrollToID('#' + sectionID, 750);
-	});
-	// scroll to top action
-	$('.scroll-top').on('click', function(event) {
-		event.preventDefault();
-		$('html, body').animate({scrollTop:0}, 'slow'); 		
-	});
-	// mobile nav toggle
-	$('#nav-toggle').on('click', function (event) {
-		event.preventDefault();
-		$('#main-nav').toggleClass("open");
-	});
+  });
 });
-// scroll function
+
+	nav.find('a').on("click", function(){
+		event.preventDefault();
+		id = $(this).attr("href");
+		scrollToID(id, 800);
+	});
+
+});
+
+
 function scrollToID(id, speed){
-	var offSet = 0;
-	var targetOffset = $(id).offset().top - offSet;
-	var mainNav = $('#main-nav');
+	var targetOffset = $(id).offset().top;
 	$('html,body').animate({scrollTop:targetOffset}, speed);
-	if (mainNav.hasClass("open")) {
-		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
-		mainNav.removeClass("open");
-	}
-}
-if (typeof console === "undefined") {
-    console = {
-        log: function() { }
-    };
 }
