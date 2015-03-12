391618aa1311247b4d65b49185616441e0467b0b
kirfog <kirfog@gmail.com>
2015-03-12 22:11:16 +0300

    startactive

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
-    
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).parent().removeClass('active');
-        })
-        $(this).parent().addClass('active');
-      
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+ 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+ 
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+ 
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
 });
 
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('a').parent().removeClass("active");
-            currLink.parent().addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+ 
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+ 
+  return false;
+});
