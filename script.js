c412d23743115037c9a96c6ca8dd475a54ac368f
kirfog <kirfog@gmail.com>
2015-03-13 00:10:58 +0300

    0.1

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	
+$('body').panelSnap();
+	
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = 100;
+ //nav.outerHeight()
+ 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  sections.each(function() {
+	  
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').parent().removeClass('active');
+      sections.removeClass('active');
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+    }
+	
+  });
+});
+
+	nav.find('a').on("click", function(){
+		event.preventDefault();
+		id = $(this).attr("href");
+		scrollToID(id, 800);
+	});
+
+});
+
+
+function scrollToID(id, speed){
+	var targetOffset = $(id).offset().top;
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+}
