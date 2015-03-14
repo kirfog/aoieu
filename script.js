46540d7c09cab0d3ea3219316b4e674fce0c309c
kirfog <kirfog@gmail.com>
2015-03-14 22:56:11 +0300

    add

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ $(window).on('scroll', function () {
 	  scolor = rgb2hex($(this).css("background-color"));
 	  iscolor = getContrast50(scolor);
 	  nav.find('a').css("color",iscolor);
-	  nav.find('a').parent().removeClass('active');
+	  //nav.find('a').parent().removeClass('active');
 	  nav.find('a').parent().addClass('white_back');
     }
   });
