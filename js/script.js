43e90c14b5713d6ea42ea1f732d7d22a696e1dcd
kirfog <kirfog@gmail.com>
2015-03-19 00:15:05 +0300

    white

--- js/script.js
+++ js/script.js
@@ -83,11 +83,11 @@ $(document).ready(function() {
 	for (i=0; i<144; i++){
 		$('#game').append('<div class="col-xs-1 card" id="c'+ i +'" class="card">'+ i +'</div>');
 	}
-	$('.card').on('click', rollover($(this)));
-	
+
+	$('.card').on('click', function(){
+\
+		$(this).css("background-color", "white");
+
 	});
 	
-	function rollover(element){
-		$(element).css("background-color", "white");
-
-	}
+});
