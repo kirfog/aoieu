b1a43eba3c0201ee31885858cd2ae5a9aa28fa41
kirfog <kirfog@gmail.com>
2015-03-18 23:32:43 +0300

    cards

--- js/script.js
+++ js/script.js
@@ -81,6 +81,6 @@ function getContrast50(hexcolor){
 $(document).ready(function() {
 
 	for (i=0; i<144; i++){
-		$('#game').append('<div class="col-xs-1" id="c'+ i +'" class="card">'+ i +'</div>');
+		$('#game').append('<div class="col-xs-1 card" id="c'+ i +'" class="card">'+ i +'</div>');
 	}
 });
