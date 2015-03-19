929b4ce3ffc38077cb4c722c5563c1e1efcfa47d
kirfog <kirfog@gmail.com>
2015-03-19 21:58:00 +0300

    hover

--- js/script.js
+++ js/script.js
@@ -90,15 +90,10 @@ $(document).ready(function() {
 
 	});
 
-	 $(document).ready(function(){
-           // set up hover panels
-           // although this can be done without JavaScript, we've attached these events
-          // because it causes the hover to be triggered when the element is tapped on a touch device
-        $('.hover').hover(function(){
+	$('.card').hover(function(){
             $(this).addClass('flip');
         },function(){
             $(this).removeClass('flip');
-        });
-    });
-	
+    	});
+
 });
