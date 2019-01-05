eb8ea4efcfbe9597ee09a131a16735dbdf2c3dbd
Kir Fog <kirfog@gmail.com>
2019-01-06 00:18:00 +0300

    <body>

--- js/script.js
+++ js/script.js
@@ -1,60 +1,52 @@
 $(document).ready(function() {
 
-	
-
-		//snapping
-	$('body').panelSnap();
-
+    $(function() {
+      $.scrollify({
+        section : ".sect",
+      });
+    });
 
 	$(".carousel-caption").animate({ 
         marginLeft: "+=9999px",
     }, 1000 );
 	
-	
-	//scroling menu
 	var sections = $('section')
   	, nav = $('nav')
   	, nav_height = 100;
  	//nav.outerHeight()
  
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-  
-  
-  sections.each(function() {
+	$(window).on('scroll', function () {
+  		var cur_pos = $(this).scrollTop();
+		sections.each(function() {
 	  
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight(),
-        menuColor = $(this).attr('menuColor');
-
-		menuColor = (typeof menuColor == typeof undefined) ? "white" : menuColor;
+    		var top = $(this).offset().top - nav_height,
+    		    bottom = top + $(this).outerHeight(),
+    		    menuColor = $(this).attr('menuColor');
 		
-    if (cur_pos >= top && cur_pos <= bottom) {
-		sections.removeClass('active');
-		$(this).addClass('active');
+				menuColor = (typeof menuColor == typeof undefined) ? "white" : menuColor;
+				
+    		if (cur_pos >= top && cur_pos <= bottom) {
+				sections.removeClass('active');
+				$(this).addClass('active');
+				
+				nav.find('a').parent().removeClass('active');
+				nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
 		
-		nav.find('a').parent().removeClass('active');
-		nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+				nav.find('a').css("color", menuColor);
+				nav.find('a[href="#'+$(this).attr('id')+'"]').css("color", "white");
+    		}
+  		});
 
-		nav.find('a').css("color", menuColor);
-		nav.find('a[href="#'+$(this).attr('id')+'"]').css("color", "white");
-    }
+  		if (cur_pos <= 300) {
+			$('#backToTop').hide(200);
+		}else {
+			$('#backToTop').show(200);
+		}
+	});
 
-  });
-
-  if (cur_pos <= 300) {
-		$('#backToTop').hide(200);
-	}else {
-		$('#backToTop').show(200);
-	}
-
-
-});//scrolling
-
-//menu_click_scroling
 	nav.find('a').on("click", function(){
 		event.preventDefault();
-		$(".navbar-collapse").collapse('hide');//hides mobile menu
+		$(".navbar-collapse").collapse('hide');
 		id = $(this).attr("href");
 		scrollToID(id, 800);
 	});
