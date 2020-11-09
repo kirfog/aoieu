0c9e00a5fc14dfa0d585df341ff80120c1771273
Kir Fog <kirfog@gmail.com>
2020-11-09 20:47:11 +0300

    livs js

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
         marginLeft: "+=9999px",
     }, 1000 );
 	
-	var sections = $('section'), nav = $('nav')	, nav_height = 100;
+	let sections = $('section'), nav = $('nav')	, nav_height = 100;
  
 	$(window).on('scroll', function () {
-  		var cur_pos = $(this).scrollTop();
+  		let cur_pos = $(this).scrollTop();
 		sections.each(function() {
 
-    		var top = $(this).offset().top - nav_height, 
+    		let top = $(this).offset().top - nav_height, 
     		    bottom = top + $(this).outerHeight(),
     		    menuColor = $(this).attr('menuColor');
     		if (cur_pos >= top && cur_pos <= bottom) {
@@ -52,15 +52,111 @@ $(document).ready(function() {
 		scrollToID(id, 350);
 	});
 
-
 	$('#backToTop').on('click',function(){
-		event.preventDefault();
 		$("html, body").animate({ scrollTop: "0" });
 	});
-
 });
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 function scrollToID(id, speed){
-	var targetOffset = $(id).offset().top;
+	let targetOffset = $(id).offset().top;
 	$('html,body').animate({scrollTop:targetOffset}, speed);
+}
+
+let x = 10;
+let y = 10;
+let n = 0;
+
+//let arr = Array(10).fill(0).map(() => Array(10).fill(0));
+//let arr = Array.from(Array(10).fill(0), () => new Array(10).fill(0))
+let cells = Array(x).fill(0).map(x => Array(y).fill(0));
+let cellsA = Array(x+2).fill(0).map(x => Array(y+2).fill(0));
+let cellsB = Array(x+2).fill(0).map(x => Array(y+2).fill(0));
+let cellsN = Array(x).fill(0).map(x => Array(y).fill(0));
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+for (let i = 0; i < x; i++) {
+	for (let j = 0; j < y; j++) {
+		cells[i][j] = randomInteger(0, 1);
+	}
+}
+
+for (let i = 0; i < x; i++) {
+	$('#live').append('<div class="row " id="row' + i + '"></div>');
+	for (let j = 0; j < y; j++) {
+		if (cells[i][j] == 0){
+		$('#row'+i).append('<div class="btn btn-warning btn-sm" id="cell' + i + j +'"><i class="fas fa-skull-crossbones"></i></div>');
+		} else {
+		$('#row'+i).append('<div class="btn btn-success btn-sm" id="cell' + i + j +'"><i class="fas fa-user"></div>');	
+		}
+}}
+
+$('#live').append('<div class="btn btn-dark btn-lg" id="run"></div>');
+
+$('#run').on('click',function(){
+	let timerId = setInterval(() => run(), 2000);
+});
+
+function run(){
+	cells = tern(cells);
+	drow(cells);
+}
+
+function tern(cells) {
+	for (let i = 0; i < x; i++) {
+		for (let j = 0; j < y; j++) {
+			cellsA[i+1][j+1] = cells[i][j];
+		}	
+	}
+
+	for (let i = 1; i < x+1; i++) {
+		for (let j = 1; j < y+1; j++) {
+			n = neib(cellsA,i,j);
+			if (cellsA[i][j] == 0 & n == 3){
+				cellsB[i][j] = 1;
+			} else if (cellsA[i][j] == 1 & n > 3){
+				cellsB[i][j] = 0;
+			} else if (cellsA[i][j] == 1 & n < 2){
+				cellsB[i][j] = 0;
+			} else {
+				cellsB[i][j] = cellsA[i][j];
+			}
+		}	
+	}
+	for (let i = 1; i < x; i++) {
+		for (let j = 1; j < y; j++) {
+                cellsN[i][j] = cellsB[i+1][j+1]
+	}}
+	console.table(cellsN);
+	return(cellsN);
+}
+
+function neib(cellsarr,i,j){
+	n = 0;
+	for (let ni = i-1; ni < i+2; ni++) {
+		for (let nj = j-1; nj < j+2; nj++) {
+			if (ni == i && nj == j) {
+			} else {
+				n = n + cellsarr[ni][nj];
+			}
+		}
+	}
+	//console.log(" Ось Х " + i + " Ось Y " + j + " Соседей " + n);
+	return n;
+}
+
+function drow(cellsarr){
+	for (let i = 0; i < x; i++) {
+		for (let j = 0; j < y; j++) {
+			if (cellsarr[i][j] == 0){
+				$('#cell'+i+j).replaceWith('<div class="btn btn-warning btn-sm" id="cell' + i + j +'"><i class="fas fa-skull-crossbones"></i></div>');
+			} else {
+				$('#cell'+i+j).replaceWith('<div class="btn btn-success btn-sm" id="cell' + i + j +'"><i class="fas fa-user"></div>');
+			}
+	}}
 }
